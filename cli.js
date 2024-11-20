2816204a6255af004b0d1488993479d3961bfa36
Chris Johnson <49479599+workeffortwaste@users.noreply.github.com>
2024-11-20 13:40:40 +0000

    Add custom chrome to cli

--- cli.js
+++ cli.js
@@ -46,7 +46,7 @@ const _yargs = yargs(hideBin(process.argv))
 /* CLI arguments config */
 const options = _yargs
   .wrap(Math.min(110, _yargs.terminalWidth()))
-  .default({ r: 'gsap', p: 'auto', c: 'libx264', o: 'video.mp4', t: 'gsap', f: 60, S: 'document', z: 1, V: '1920x1080', v: 'auto', E: '"-pix_fmt yuv420p -crf 18"', q: true, h: true })
+  .default({ r: 'gsap', p: 'auto', c: 'libx264', o: 'video.mp4', t: 'gsap', f: 60, S: 'document', z: 1, V: '1920x1080', v: 'auto', E: '"-pix_fmt yuv420p -crf 18"', q: true, h: true, chrome: false })
   .usage('$0 <url>', 'Export GreenSock (GSAP) animation to video')
   .describe('s', '[browser] Custom script')
   .describe('S', '[browser] DOM selector')
@@ -56,6 +56,7 @@ const options = _yargs
   .describe('i', '[browser] Info only')
   .describe('frame-start', '[browser] Start frame')
   .describe('frame-end', '[browser] End frame')
+  .describe('chrome', '[browser] Use the system installed Chrome')
   .describe('a', '[browser] Frame advance method')
   .describe('h', '[browser] Headless mode')
   .describe('p', '[video] Auto padding color')
@@ -83,8 +84,8 @@ const options = _yargs
   .alias('q', 'verbose')
   .alias('h', 'headless')
   .number(['f', 'z'])
-  .boolean(['i', 'q', 'h'])
   .string(['e', 'E', 'S', 's', 'o', 't', 'v', 'V', 'c', 'p'])
+  .boolean(['i', 'q', 'h', 'chrome'])
   .epilogue('For more information visit documentation at: \nhttp://github.com/workeffortwaste/gsap-video-export')
   .argv
 
